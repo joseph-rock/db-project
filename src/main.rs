25d77c1878981c995ad0dd6e41f4b906d7db5b82
Joseph <josephrock225@gmail.com>
2025-12-12 15:22:19 -0800

    New tables (broken)

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 mod unit;
 
+use rusqlite::{Connection, Error, Result};
 use unit::*;
-use rusqlite::{Connection, Result, Error};
 
 #[derive(Debug)]
 struct Ingredient {
@@ -19,67 +19,87 @@ struct Recipie {
 fn main() -> Result<()> {
     // Init connection and create tables
     let conn = Connection::open_in_memory()?;
-    let _ = init_tables(&conn);
+    let foo = init_tables(&conn);
+    dbg!(foo);
 
-    // Add milk & cereal
-    let milk = Ingredient {
-        id: 0,
-        name: "Milk".to_string(),
-        unit: Unit { 
-            name: UnitName::Gallon, 
-            amount: 1.0,
-        },
-    };
-    let _ = add_ingredient(&conn, &milk);
-
-    let cereal = Ingredient {
-        id: 1,
-        name: "Wheaties".to_string(),
-        unit: Unit { 
-            name: UnitName::Ounce, 
-            amount: 15.6,
-        },
-    };
-    let _ = add_ingredient(&conn, &cereal);
-
-    let ingredients = select_all_ingredients(&conn).expect("trust me bro");
-    dbg!(ingredients);
-
-    // Update milk amount
-    let less_milk = Ingredient {
-        id: 0,
-        name: "Milk".to_string(),
-        unit: Unit { 
-            name: UnitName::Gallon, 
-            amount: 1.5,
-        },
-    };
-
-    let _ = update_ingredient(&conn, &less_milk);
-    let ingredients = select_all_ingredients(&conn).expect("trust me bro");
-    dbg!(ingredients);
-
-    // Get single ingredient
-    let get_cereal = select_ingredient(&conn, "Wheaties").expect("trust me bro");
-    dbg!(&get_cereal);
-
-    // Use an amount of an ingredient
-    let _ = use_ingredient(&conn, "Wheaties", 2.0);
-    let get_cereal = select_ingredient(&conn, "Wheaties").expect("trust me bro");
-    dbg!(&get_cereal);
+    // // Add milk & cereal
+    // let milk = Ingredient {
+    //     id: 0,
+    //     name: "Milk".to_string(),
+    //     unit: Unit {
+    //         name: UnitName::Gallon,
+    //         amount: 1.0,
+    //     },
+    // };
+    // let _ = add_ingredient(&conn, &milk);
+    //
+    // let cereal = Ingredient {
+    //     id: 1,
+    //     name: "Wheaties".to_string(),
+    //     unit: Unit {
+    //         name: UnitName::Ounce,
+    //         amount: 15.6,
+    //     },
+    // };
+    // let _ = add_ingredient(&conn, &cereal);
+    //
+    // let ingredients = select_all_ingredients(&conn).expect("trust me bro");
+    // dbg!(ingredients);
+    //
+    // // Update milk amount
+    // let less_milk = Ingredient {
+    //     id: 0,
+    //     name: "Milk".to_string(),
+    //     unit: Unit {
+    //         name: UnitName::Gallon,
+    //         amount: 1.5,
+    //     },
+    // };
+    //
+    // let _ = update_ingredient(&conn, &less_milk);
+    // let ingredients = select_all_ingredients(&conn).expect("trust me bro");
+    // dbg!(ingredients);
+    //
+    // // Get single ingredient
+    // let get_cereal = select_ingredient(&conn, "Wheaties").expect("trust me bro");
+    // dbg!(&get_cereal);
+    //
+    // // Use an amount of an ingredient
+    // let _ = use_ingredient(&conn, "Wheaties", 2.0);
+    // let get_cereal = select_ingredient(&conn, "Wheaties").expect("trust me bro");
+    // dbg!(&get_cereal);
 
     Ok(())
 }
 
-fn init_tables(conn: &Connection) -> Result<usize, Error> {
-    conn.execute(
-        "CREATE TABLE ingredients (
+fn init_tables(conn: &Connection) -> Result<()> {
+    conn.execute_batch(
+        "
+        CREATE TABLE ingredient(
+            id INTEGER PRIMARY KEY,
+            name TEXT NOT NULL);
+
+        CREATE TABLE recipe(
             id INTEGER PRIMARY KEY,
             name TEXT NOT NULL,
-            amount FLOAT NOT NULL,
-            unit TEXT NOT NULL
-        )",
-        (),
+            description TEXT NOT NULL);
+
+        CREATE TABLE inventory(
+            id INTEGER PRIMARY KEY,
+            ingredient_id INTEGER NOT NULL,
+            amount INTEGER NOT NULL,
+            amount_unit TEXT NOT NULL,
+            FOREIGN KEY (ingredient_id) REFERENCES ingredient(id));
+
+        CREATE TABLE recipe_ingredient(
+            ingredient_id INTEGER NOT NULL,
+            recipe_id INTEGER NOT NULL,
+            amount INTEGER NOT NULL,
+            amount_unit TEXT NOT NULL,
+            FOREIGN KEY (ingredient_id) REFERENCES ingredient(id),
+            FOREIGN KEY (recipe_id) REFERENCES recipe(id),
+            PRIMARY KEY (ingredient_id, recipe_id));
+        ",
     )
 }
 
@@ -87,7 +107,11 @@ fn add_ingredient(conn: &Connection, ingredient: &Ingredient) -> Result<usize, E
     conn.execute(
         "INSERT INTO ingredients (name, amount, unit) 
             VALUES (?1, ?2, ?3);",
-        (&ingredient.name, &ingredient.unit.amount, &ingredient.unit.name.to_string()),
+        (
+            &ingredient.name,
+            &ingredient.unit.amount,
+            &ingredient.unit.name.to_string(),
+        ),
     )
 }
 
