c74076055cb00c9836e9bfd30a652f219c6cb06a
Joseph <josephrock225@gmail.com>
2026-01-03 16:53:43 -0800

    rewrite

--- src/main.rs
+++ src/main.rs
@@ -9,17 +9,17 @@ struct Ingredient {
     name: String,
 }
 
-#[derive(Debug)]
-struct IngredientUnit {
-    ingredient: Ingredient,
-    unit: Unit,
-}
-
-struct Recipe {
-    id: i32,
-    name: String,
-    ingredients: Vec<IngredientUnit>,
-}
+// #[derive(Debug)]
+// struct Inventory {
+//     ingredient: Ingredient,
+//     unit: Unit,
+// }
+//
+// struct Recipe {
+//     id: i32,
+//     name: String,
+//     ingredients: Vec<Inventory>,
+// }
 
 fn main() -> Result<()> {
     // Init connection and create tables
@@ -32,138 +32,77 @@ fn main() -> Result<()> {
 fn init_tables(conn: &Connection) -> Result<()> {
     conn.execute_batch(
         "
-            create table ingredients (
-              id integer primary key,
-              name text not null unique
-            ) strict;
+        create table if not exists ingredients (
+          id integer primary key,
+          name text not null
+        ) strict;
 
-            create table inventory (
-              id integer primary key,
-              amount integer not null,
-              unit integer not null references units,
-              ingredient integer not null refrences ingredients
-            ) strict;
+        create table if not exists units (
+          id integer primary key,
+          name text not null
+        ) strict;
 
-            create table recipes (
-              id integer primary key,
-              name text not null,
-              description text not null
-            ) strict;
+        create table if not exists recipes (
+          id integer primary key,
+          name text not null,
+          description text not null
+        ) strict;
 
-            create table recipe_ingredients (
-              ingredient integer not null references ingredients,
-              recipe integer not null references recipes,
-              amount integer not null,
-              unit integer not null references units,
-              PRIMARY KEY (ingredient, recipe)
-            ) strict;
+        create table if not exists inventory (
+          id integer primary key,
+          amount integer not null,
+          unit integer not null references units,
+          ingredient integer not null references ingredients
+        ) strict;
 
-            create table units (
-              id integer primary key,
-              name text not null
-            ) strict;
+        create table if not exists recipe_ingredients (
+          ingredient integer not null references ingredients,
+          recipe integer not null references recipes,
+          amount integer not null,
+          unit integer not null references units,
+          PRIMARY KEY (ingredient, recipe)
+        ) strict;
         ",
     )
 }
 
-fn add_ingredient(conn: &Connection, ingredient: &Ingredient) -> Result<usize, Error> {
-    let mut inventory_stmt = conn.prepare("INSERT INTO ingredient(name) VALUES (?1);")?;
-    inventory_stmt.execute(params![&ingredient.name])
+fn insert_ingredient(conn: &Connection, name: &str) -> Result<usize, Error> {
+    let mut stmt = conn.prepare("insert into ingredients (name) values (?1);")?;
+    stmt.execute(params![name])
 }
 
-fn get_ingredient(conn: &Connection, ingredient: &Ingredient) -> Result<Ingredient, Error> {
-    let mut stmt = conn.prepare("SELECT id, name FROM ingredient WHERE name = ?1")?;
-    stmt.query_one([&ingredient.name], |row| {
+// query_one
+//  returns Err(QueryReturnedMoreThanOneRow)
+//  returns Err(QueryReturnedNoRows)
+fn select_ingredient(conn: &Connection, name: &str) -> Result<Ingredient, Error> {
+    let mut stmt = conn.prepare("select id, name from ingredients where name = ?1;")?;
+    stmt.query_one(params![name], |row| {
         let id = row.get(0)?;
         let name = row.get(1)?;
         Ok(Ingredient { id, name })
     })
 }
 
-fn add_inventory(conn: &Connection, inventory: &IngredientUnit) -> Result<usize, Error> {
-    // try add ingredient first
-    // *could* throw away return, expect to work or return "ConstraintViolation" which is fine
-    // TODO: should handle other errors
-    let _ = add_ingredient(&conn, &inventory.ingredient);
-    let ingredient = get_ingredient(&conn, &inventory.ingredient)?;
-    let amount = &inventory.unit.amount;
-    let amount_unit = &inventory.unit.name.to_string();
+// This table should be pre-populated -- no one is entering units
+fn insert_unit(conn: &Connection, name: &str) -> Result<usize, Error> {
+    let mut stmt = conn.prepare("insert into units (name) values (?1);")?;
+    stmt.execute(params![name])
+}
 
-    let mut inventory_stmt = conn.prepare(
+fn insert_inventory(conn: &Connection, inventory: (usize, String, String)) -> Result<usize, Error> {
+    // if unit does not exist, insert_unit()
+
+    // if ingredient does not exist, insert_ingredient()
+    let mut stmt = conn.prepare(
         "
-        INSERT INTO inventory(id, amount, amount_unit) 
-        VALUES (?1, ?2, ?3);
+        insert into inventory (amount, unit, ingredient)
+        values (
+            ?1, 
+            (select id from units where name = ?2), 
+            (select id from ingredients where name = ?3)
+          );
         ",
     )?;
-    inventory_stmt.execute(params![ingredient.id, amount, amount_unit])
+    stmt.execute(params![inventory.0, inventory.1, inventory.2])
 }
 
-fn get_all_inventory(conn: &Connection) -> Result<Vec<IngredientUnit>, Error> {
-    let mut stmt = conn.prepare(
-        "SELECT ingredient.id, ingredient.name, inventory.amount, inventory.amount_unit
-            FROM inventory
-            JOIN ingredient ON ingredient.id = inventory.id;",
-    )?;
-    stmt.query_map([], |row| {
-        let id = row.get(0)?;
-        let name = row.get(1)?;
-        let amount = row.get::<usize, f64>(2)?;
-        let unit_str = row.get::<usize, String>(3)?;
-        Ok(IngredientUnit {
-            ingredient: Ingredient { id, name },
-            unit: Unit {
-                name: UnitName::from_string(&unit_str).unwrap(),
-                amount,
-            },
-        })
-    })?
-    .collect()
-}
-
-// TODO: this naming is awful
-fn get_one_inventory(conn: &Connection, name: &str) -> Result<IngredientUnit, Error> {
-    let mut stmt = conn.prepare(
-        "SELECT ingredient.id, ingredient.name, inventory.amount, inventory.amount_unit
-            FROM inventory
-            JOIN ingredient ON ingredient.id = inventory.id
-            WHERE name = ?1;",
-    )?;
-    stmt.query_one([name], |row| {
-        let id = row.get(0)?;
-        let ingredient_name = row.get(1)?;
-        let amount = row.get::<usize, f64>(2)?;
-        let unit_str = row.get::<usize, String>(3)?;
-        let unit_name = UnitName::from_string(&unit_str).unwrap();
-        Ok(IngredientUnit {
-            ingredient: Ingredient {
-                id,
-                name: ingredient_name,
-            },
-            unit: Unit {
-                name: unit_name,
-                amount,
-            },
-        })
-    })
-}
-
-fn update_inventory(conn: &Connection, inventory: &IngredientUnit) -> Result<usize, Error> {
-    conn.execute(
-        "UPDATE inventory
-            SET amount = ?1
-            WHERE name = ?2;",
-        (&inventory.unit.amount, &inventory.ingredient.name),
-    )
-}
-
-fn get_one_recipe(conn: &Connection, name: &str) -> Result<Recipe, Error> {
-    let mut stmt = conn.prepare(
-        "SELECT ingredient.name, recipe.name, recipe_ingredient.amount, recipe_ingredient.amount_unit
-        FROM recipe_ingredient
-        JOIN ingredient ON ingredient.id = recipe_ingredient.ingredient_id
-        JOIN recipe ON recipe.id = recipe_ingredient.recipe_id
-        WHERE recipe.name = ?1;"
-        )?;
-
-    todo!()
-}
